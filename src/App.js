313008dbc4730629bca4199a815e61cdda406aa6
Holly <holly.fanthorpe@iceland.co.uk>
2021-08-07 16:04:46 +0100

    feat: created nav bar for mobile and desktop

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import { NavBar } from "./components/NavBar/NavBar";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar />
-      </header>
-      <footer>
-      <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-      </footer>
-    </div>
-  );
+		<div className="App">
+			<header className="App-header">
+				<NavBar />
+			</header>
+			{/* <footer>
+				<div>
+					Icons made by{" "}
+					<a
+						href="https://www.flaticon.com/authors/pixel-perfect"
+						title="Pixel perfect"
+					>
+						Pixel perfect
+					</a>{" "}
+					and{" "}
+					<a href="https://www.flaticon.com/authors/xnimrodx" title="xnimrodx">
+						xnimrodx
+					</a>{" "}
+					from{" "}
+					<a href="https://www.flaticon.com/" title="Flaticon">
+						www.flaticon.com
+					</a>
+				</div>
+			</footer> */}
+		</div>
+	);
 }
 
 export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import { useMedia } from 'use-media';
+import { useMedia } from "use-media";
+import { ReactComponent as BurgerMenuIcon } from "../../svgs/menu-icon.svg";
+import { ReactComponent as CloseIcon } from "../../svgs/cancel-icon.svg";
 
-import styles from './NavBar.module.css'
+import styles from "./NavBar.module.css";
 
 export const NavBar = () => {
-    const isMobile = useMedia({maxWidth: '600px'})
+	const [showMobileLinks, setShowMobileLinks] = useState(false);
 
-    const renderNavBar = () => <div>Nav bar</div>
+	const isMobile = useMedia({ maxWidth: "600px" });
 
-    return (
-        <div className={styles.navBar}>
-            {isMobile ? 'Burger Icon' : renderNavBar()}
-        </div>
-    )
-}
+	const renderNavBar = () => {
+		return (
+			<div className={styles.navBarLinks}>
+				<span>Details</span>
+				<span>Destination</span>
+				<span>Getting There</span>
+				<span>Accommodation</span>
+				<span>RSVP</span>
+			</div>
+		);
+	};
 
-
-
-
+	return (
+		<div className={showMobileLinks ? styles.navBarMobile : styles.navBar}>
+			{isMobile ? (
+				<>
+					{!showMobileLinks ? (
+						<button
+							className={styles.burgerMenuButton}
+							onClick={() => setShowMobileLinks(true)}
+						>
+							<BurgerMenuIcon className={styles.burgerMenuIcon} />
+						</button>
+					) : (
+						<>
+							<button
+								className={styles.closeButton}
+								onClick={() => setShowMobileLinks(false)}
+							>
+								<CloseIcon className={styles.closeButtonIcon} />
+							</button>
+							{renderNavBar()}
+						</>
+					)}
+				</>
+			) : (
+				renderNavBar()
+			)}
+		</div>
+	);
+};
