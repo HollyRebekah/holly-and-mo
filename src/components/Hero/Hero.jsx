c83c472169eedb0dafc2bde64400e8edf4ca7d22
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2021-08-07 18:31:05 +0100

    feat: added text to hero image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,5 +3,12 @@ import React from 'react';
 import styles from './Hero.module.css';
 
 export const Hero = () => {
-  return <div className={styles.heroImage}></div>;
+  return (
+    <div className={styles.heroImage}>
+      <h1 className={styles.heroText}>
+        <span className={styles.textLarge}>Holly & Mo</span>
+        <span className={styles.textSmall}>are getting married</span>
+      </h1>
+    </div>
+  );
 };
